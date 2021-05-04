7c9522ff9ee0247d2490c896f6fabc51785d835a
Eduardo Gomez <edgome@microsoft.com>
2021-05-04 14:28:17 -0500

    ag

---
+++ azuredeploy.ps1
@@ -0,0 +1,14 @@
+$localpath = "{your local directory}"
+$templatefile = $localpath + "azuredeploy.json"
+$templateparamfile = $localpath + "azuredeploy.parameters.json"
+$params = get-content $templateparamfile | ConvertFrom-Json
+$prefix = $params.parameters.resourcenameprefix.value
+$rgname = $prefix + "-rg"
+$location = "East US"
+$rg = get-azresourcegroup -location $location -name $rgname
+if ($null -eq $rg)
+{
+    new-azresourcegroup -location $location -name $rgname
+}
+New-AzResourceGroupDeployment -ResourceGroupName $rgname -TemplateFile $templateFile -TemplateParameterFile $templateparamfile 
+write-host "ARM Deployment Complete"
